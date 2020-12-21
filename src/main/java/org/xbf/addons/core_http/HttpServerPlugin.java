2dc34631799b7b3862b6c03e62cc9c1f6575eb59
BL19 <30739672+BL19@users.noreply.github.com>
2020-12-21 17:35:15 +0100

    Change Version

--- src/main/java/org/xbf/addons/core_http/HttpServerPlugin.java
+++ src/main/java/org/xbf/addons/core_http/HttpServerPlugin.java
@@ -1,18 +1,13 @@
 package org.xbf.addons.core_http;
 
-import java.awt.Color;
-
 import org.xbf.core.XBF;
-import org.xbf.core.XVI;
-import org.xbf.core.Messages.Response;
-import org.xbf.core.Messages.RichResponse;
 import org.xbf.core.Plugins.DependsOn;
 import org.xbf.core.Plugins.PluginVersion;
 import org.xbf.core.Plugins.XPlugin;
 import org.xbf.core.Plugins.XervinJavaPlugin;
 
 @XPlugin(name="core_http", displayname="XBF Core HTTP Server", description="An HTTP Server with addon support for XBF")
-@PluginVersion(currentVersion="1.0.0")
+@PluginVersion(currentVersion="1.0.1")
 @DependsOn(pluginName="xbf", minimumVersion="0.0.14")
 public class HttpServerPlugin extends XervinJavaPlugin {
 
